a843a4e8297aefc168f4e4f09ff1c0af4b487c96
0ri0 <oriu5528@gmail.com>
2021-04-24 22:47:48 +0300

    idk

--- scripts/quadratic-formula-calculator.js
+++ scripts/quadratic-formula-calculator.js
@@ -1,20 +1,18 @@
 document.getElementById("submit").onclick = function() {
-    let a = document.getElementById("a").innerText;
-    let b = document.getElementById("a").innerText;
-    let c = document.getElementById("a").innerText;
+    let a = parseFloat(document.getElementById("a").innerText);
+    let b = parseFloat(document.getElementById("a").innerText);
+    let c = parseFloat(document.getElementById("a").innerText);
 
     let dicriminator = b * b - 4 * a * c;
 
     if (discriminator < 0) {
         document.getElementById("x1").innerText = "x₁ ∉ ℝ";
         document.getElementById("x2").innerText = "x₂ ∉ ℝ";
-    } 
-    else if (discriminator = 0) {
-        document.getElementById("x1").innerText = "x₁ = " + (-b / 2 * a);
-        document.getElementById("x2").innerText = "x₂ = " + (-b / 2 * a);
-    }
-    else if (discriminator > 0)
-    {
-        
+    } else if (discriminator = 0) {
+        document.getElementById("x1").innerText = "x₁ = " + (-b / (2 * a));
+        document.getElementById("x2").innerText = "x₂ = " + (-b / (2 * a));
+    } else if (discriminator > 0) {
+        document.getElementById("x1").innerText = "x₁ = " + ((-b + Math.sqrt(discriminator)) / (2 * a));
+        document.getElementById("x2").innerText = "x₂ = " + ((-b - Math.sqrt(discriminator)) / (2 * a));
     }
 }
