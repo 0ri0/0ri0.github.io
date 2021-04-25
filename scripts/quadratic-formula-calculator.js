de869768de6c462df0d8b331f78e90d347d07023
0ri0 <oriu5528@gmail.com>
2021-04-25 22:14:43 +0300

    yes

--- scripts/quadratic-formula-calculator.js
+++ scripts/quadratic-formula-calculator.js
@@ -1,16 +1,16 @@
 document.getElementById("submit").onclick = function() {
-    let a = parseFloat(document.getElementById("a").innerText);
-    let b = parseFloat(document.getElementById("b").innerText);
-    let c = parseFloat(document.getElementById("c").innerText);
-
+    let a = parseFloat(document.getElementById("a").value);
+    let b = parseFloat(document.getElementById("b").value);
+    let c = parseFloat(document.getElementById("c").value);
     let discriminator = b * b - 4 * a * c;
-
     if (discriminator < 0) {
         document.getElementById("x1").innerText = "x₁ ∉ ℝ";
         document.getElementById("x2").innerText = "x₂ ∉ ℝ";
-    } else if (discriminator = 0) {
+        alert("<0");
+    } else if (discriminator == 0) {
         document.getElementById("x1").innerText = "x₁ = " + (-b / (2 * a));
         document.getElementById("x2").innerText = "x₂ = " + (-b / (2 * a));
+        alert("0");
     } else if (discriminator > 0) {
         document.getElementById("x1").innerText = "x₁ = " + ((-b + Math.sqrt(discriminator)) / (2 * a));
         document.getElementById("x2").innerText = "x₂ = " + ((-b - Math.sqrt(discriminator)) / (2 * a));
