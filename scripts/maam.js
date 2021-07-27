84124c19b1cd5dac5367c5024be5d17a5115fba9
0ri0 <oriu5528@gmail.com>
2021-07-27 22:00:45 +0300

    Update maam.js

--- scripts/maam.js
+++ scripts/maam.js
@@ -1,7 +1,7 @@
 document.getElementById("remove").onclick = function()
 {   
     let input = document.getElementById("input").value;
-    document.getElementById("results").innerHTML = 100*input/117;
+    document.getElementById("results").innerHTML = input/1.17;
 }
 document.getElementById("add").onclick = function()
 {
